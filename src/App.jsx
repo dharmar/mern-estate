58baee98ef4c0ec03ae2f55cb30f11136170e040
dharmar <dharmaraws@gmail.com>
2025-04-25 20:27:00 +0530

    React and Tailwind css installation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-800 text-4xl">Welcome to Dr.Sahand course</h1>
+    </div>
+  );
+};
+
+export default App;

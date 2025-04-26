6d1be53b823a7b1ef0da599874b0dc3352f890a1
dharmar <dharmaraws@gmail.com>
2025-04-26 09:26:50 +0530

    Pages and Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignOut from "./pages/SignOut.jsx";
+import Profile from "./pages/Profile.jsx";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-800 text-4xl">Welcome to Dr.Sahand course</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signout" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
